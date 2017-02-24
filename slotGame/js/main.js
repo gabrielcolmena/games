def52919ed12c287ec8f76e021171a9ac9bd0b80
gabrielcolmena <gbrieldcg@gmail.com>
2017-02-23 23:13:51 -0400

    Gaming

--- slotGame/js/main.js
+++ slotGame/js/main.js
@@ -59,44 +59,59 @@ var loaderState = {
         this.load.image('clean2', './assets/img/clean2.png');
         this.load.image('bet', './assets/img/bet.png');
         this.load.image('tableId', './assets/img/tableId.png');
+        this.load.image('opacity', './assets/img/opacity.png');
         this.load.image('about', './assets/img/about.png');
         this.load.image('about-hover', './assets/img/about-hover.png');
         this.load.image('init-frame', './assets/img/init-frame.png');
 
         //loading sprites
+        this.load.spritesheet('numbers', './assets/sprites/n.png', 50, 59, 10);
         this.load.spritesheet('ribbon1', './assets/sprites/ribbons/ribbon1.png', 120, 560, 5);
         this.load.spritesheet('ribbon2', './assets/sprites/ribbons/ribbon2.png', 120, 560, 5);
         this.load.spritesheet('ribbon3', './assets/sprites/ribbons/ribbon3.png', 120, 560, 5);
         this.load.spritesheet('hamburguer_1', './assets/sprites/icons/hamburguer/hamburguer1.png', 120, 400, 30);
+        this.load.spritesheet('winning_hamburguer', './assets/sprites/winnings/hamburguer.png', 560, 400, 60);
         this.load.spritesheet('hamburguer_2', './assets/sprites/icons/hamburguer/hamburguer2.png', 120, 400, 30);
         this.load.spritesheet('hamburguer_3', './assets/sprites/icons/hamburguer/hamburguer3.png', 120, 400, 30);
         this.load.spritesheet('hotdog_1', './assets/sprites/icons/hotdog/hotdog1.png', 120, 400, 30);
+        this.load.spritesheet('winning_hotdog', './assets/sprites/winnings/hotdog.png', 560, 400, 60);
         this.load.spritesheet('hotdog_2', './assets/sprites/icons/hotdog/hotdog2.png', 120, 400, 30);
         this.load.spritesheet('hotdog_3', './assets/sprites/icons/hotdog/hotdog3.png', 120, 400, 30);
         this.load.spritesheet('logo_1', './assets/sprites/icons/logo/logo1.png', 120, 400, 30);
+        this.load.spritesheet('winning_logo', './assets/sprites/winnings/logo.png', 560, 400, 60);
         this.load.spritesheet('logo_2', './assets/sprites/icons/logo/logo2.png', 120, 400, 30);
         this.load.spritesheet('logo_3', './assets/sprites/icons/logo/logo3.png', 120, 400, 30);
         this.load.spritesheet('pizza_1', './assets/sprites/icons/pizza/pizza1.png', 120, 400, 30);
+        this.load.spritesheet('winning_pizza', './assets/sprites/winnings/pizza.png', 560, 400, 60);
         this.load.spritesheet('pizza_2', './assets/sprites/icons/pizza/pizza2.png', 120, 400, 30);
         this.load.spritesheet('pizza_3', './assets/sprites/icons/pizza/pizza3.png', 120, 400, 30);
         this.load.spritesheet('cake_1', './assets/sprites/icons/cake/cake1.png', 120, 400, 30);
+        this.load.spritesheet('winning_cake', './assets/sprites/winnings/cake.png', 560, 400, 60);
         this.load.spritesheet('cake_2', './assets/sprites/icons/cake/cake2.png', 120, 400, 30);
         this.load.spritesheet('cake_3', './assets/sprites/icons/cake/cake3.png', 120, 400, 30);
         this.load.spritesheet('coffee_1', './assets/sprites/icons/coffee/coffee1.png', 120, 400, 30);
+        this.load.spritesheet('winning_coffee', './assets/sprites/winnings/coffee.png', 560, 400, 60);
         this.load.spritesheet('coffee_2', './assets/sprites/icons/coffee/coffee2.png', 120, 400, 30);
         this.load.spritesheet('coffee_3', './assets/sprites/icons/coffee/coffee3.png', 120, 400, 30);
         this.load.spritesheet('cookies_1', './assets/sprites/icons/cookies/cookies1.png', 120, 400, 30);
+        this.load.spritesheet('winning_cookies', './assets/sprites/winnings/cookies.png', 560, 400, 60);
         this.load.spritesheet('cookies_2', './assets/sprites/icons/cookies/cookies2.png', 120, 400, 30);
         this.load.spritesheet('cookies_3', './assets/sprites/icons/cookies/cookies3.png', 120, 400, 30);
         this.load.spritesheet('fries_1', './assets/sprites/icons/fries/fries1.png', 120, 400, 30);
+        this.load.spritesheet('winning_fries', './assets/sprites/winnings/fries.png', 560, 400, 60);
         this.load.spritesheet('fries_2', './assets/sprites/icons/fries/fries2.png', 120, 400, 30);
         this.load.spritesheet('fries_3', './assets/sprites/icons/fries/fries3.png', 120, 400, 30);
         this.load.spritesheet('ice_cream_1', './assets/sprites/icons/ice_cream/ice_cream1.png', 120, 400, 30);
+        this.load.spritesheet('winning_ice_cream', './assets/sprites/winnings/ice_cream.png', 560, 400, 60);
         this.load.spritesheet('ice_cream_2', './assets/sprites/icons/ice_cream/ice_cream2.png', 120, 400, 30);
         this.load.spritesheet('ice_cream_3', './assets/sprites/icons/ice_cream/ice_cream3.png', 120, 400, 30);
+        this.load.spritesheet('again_button', './assets/sprites/play_again_button/again.png', 147, 33, 10);
+        this.load.spritesheet('confetti', './assets/sprites/confetti/confetti.png', 560, 400, 30);
+        this.load.spritesheet('big_win', './assets/sprites/big_win/big_win.png', 277, 273, 40);
 
         //loading sounds
         this.load.audio('spinnin', './assets/sounds/spinnin.mp3');
+        this.load.audio('crowd', './assets/sounds/crowd.mp3');
 
     },
     create: function(){
@@ -110,6 +125,26 @@ var loaderState = {
 var tokensState = {
 
     playIdsTable: ['','coffee', 'cookies', 'ice_cream', 'cake', 'fries', 'hotdog', 'hamburguer', 'pizza', 'logo'],
+    prizesTable: [0, 2, 4, 8, 10, 25, 100, 250, 500, 2500, 5000],
+
+    numbersArray: [
+    [1,2,3,4,5,6,7,8,9,0],
+    [2,3,4,5,6,7,8,9,0,1],
+    [3,4,5,6,7,8,9,0,1,2],
+    [4,5,6,7,8,9,0,1,2,3],
+    [5,6,7,8,9,0,1,2,3,4],
+    [6,7,8,9,0,1,2,3,4,5],
+    [7,8,9,0,1,2,3,4,5,6],
+    [8,9,0,1,2,3,4,5,6,7], 
+    [9,0,1,2,3,4,5,6,7,8], 
+    [0,1,2,3,4,5,6,7,8,9],
+    ],
+
+    pingPongArray: [
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,28,27,26,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40],//normaloop
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26],//cooffeloop
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]//logo loop
+    ],
 
     create: function(){
 
@@ -129,6 +164,7 @@ var tokensState = {
         var sound_on = this.add.image(40, 20, 'sound_on');
         var sound_off = this.add.image(40, 50, 'sound_off');
 
+        //game.sound.mute = true;
         if(!game.sound.mute)sound_off.alpha = 0;
         else sound_on.alpha = 0;
         sound_on.inputEnabled = true;
@@ -206,6 +242,7 @@ var tokensState = {
 
         button_spin.inputEnabled = true;
         button_spin.events.onInputDown.add(function(){
+            game.add.audio('spinnin').play();
             __tokensToPlayJSON = playGame(__gameId, __tokensToPlay);
             initFrame.alpha = 0;
             ribbon1.alpha = 1;
@@ -237,6 +274,58 @@ var tokensState = {
                     ribbon3.loadTexture(key +'_'+ treeArray[2], 0, false);
                     ribbon3.animations.add('finished');
                     ribbon3.animations.play('finished', 20, false);
+                    ribbon1.events.onAnimationComplete.add(function(){
+                        var opacity = tokensState.add.image(0 , 0, 'opacity');
+                        var winning = tokensState.add.sprite(0, 0, 'winning_' + key);
+                        var index = __tokensToPlayJSON.playId == 9 ? 2 : __tokensToPlayJSON.playId == 1 ? 1 : 0
+                        winning.animations.add('winning', tokensState.pingPongArray[index]);
+                        winning.animations.play('winning', 20, false);
+                        winning.events.onAnimationComplete.add(function(){
+                            var againButton = game.add.sprite(35, 350, 'again_button');
+                            againButton.animations.add('click');
+
+                            againButton.inputEnabled = true;
+
+                            againButton.events.onInputOver.add(function(){
+                                againButton.animations.add('hover', [4]);
+                                againButton.animations.play('hover');
+                            });
+
+                            againButton.events.onInputOut.add(function(){
+                                againButton.animations.add('no-hover', [0]);
+                                againButton.animations.play('no-hover');
+                            });
+                            //setting the event for the start button
+                            againButton.events.onInputDown.add(function(){
+                                againButton.animations.play('click', 15, false);
+                                setTimeout(function(){
+                                    __tokensToPlay = 1;
+                                    game.state.start('tokensState');
+                                }, 500);
+                            }, this);
+                        });
+                        var amount = tokensState.prizesTable[__tokensToPlayJSON.playId];
+                        var n = ("" + amount).split("");
+                        var distance = amount.toString().length == 4 ? 140 : amount.toString().length == 3 ? 132 : amount.toString().length == 2 ? 126 : 120;
+                        for(var i = 0; i < amount.toString().length; i++){
+                            var number = tokensState.add.sprite((parseInt(i * 34) + distance) - (25 * (n.length)), game.world.centerY + 80, 'numbers');
+                            number.scale.setTo(.8, .8);
+                            number.animations.add('animation', tokensState.numbersArray[parseInt(n[i])]);
+                            number.animations.play('animation', 20, false);
+                        }
+                        if(__tokensToPlayJSON.playId == 9){
+                            var bigwin = game.add.sprite(40,120,'big_win');
+                            bigwin.scale.setTo(.5, .5);
+                            bigwin.animations.add('big_win');
+                            bigwin.animations.play('big_win', 20, false);
+                            var confetti = tokensState.add.sprite(0, 0, 'confetti');
+                            confetti.animations.add('hurray');
+                            confetti.animations.play('hurray', 15, false);
+                            setTimeout(function(){
+                                game.add.audio('crowd').play();
+                            },500);
+                        }
+                    })
                 }
             });
         });
